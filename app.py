24a03b83cc6660b7695ecb6300a0904f140d8ddd
Saksham Srivastava <saksham.sri23@gmail.com>
2021-05-14 23:27:32 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask,send_from_directory
+from flask import Flask,send_from_directory,
 import requests
 from flask import request
 from flask import json
 from os import environ
+from flask_cors import cross_origin
 import time
 app=Flask(__name__,static_url_path='',static_folder='/build')
 url=environ.get('url')
@@ -14,6 +15,7 @@ def home():
     return 'hello world'
 
 @app.route('/get',methods=['POST','GET'])
+@cross_origin()
 def sendList():
     payload={'q':request.json['q'],'app_id':API_ID,'app_key':API_KEY,'health':request.json['health']}
     r=requests.get(url,params=payload)
